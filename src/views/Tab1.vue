77825fd7400183a1cdab3588a0ad1e72728aacad
krisnahumanis17 <68936615+krisnahumanis17@users.noreply.github.com>
2022-11-02 07:01:55 +0700

    Add files via upload

---
+++ src/views/Tab1.vue
@@ -0,0 +1,146 @@
+<template>
+  <ion-page>
+    <ion-content :fullscreen="true">
+      <div>
+        <ion-grid>
+          <ion-row>
+            <ion-col class="ion-padding">
+              <h1>Home</h1>
+            </ion-col>
+          </ion-row>
+
+          <ion-row>
+            <ion-col>
+              <ion-card color="dark">
+                <ion-card-header>
+                  <ion-card-title>Kolam</ion-card-title>
+                </ion-card-header>
+                <ion-card-content>
+                  <h1><strong>10</strong></h1>
+                </ion-card-content>
+              </ion-card>
+            </ion-col>
+            <ion-col>
+              <ion-card color="dark">
+                <ion-card-header>
+                  <ion-card-title>Kolam Aktif</ion-card-title>
+                </ion-card-header>
+                <ion-card-content>
+                  <h1><strong>8</strong></h1>
+                </ion-card-content>
+              </ion-card>
+            </ion-col>
+          </ion-row>
+          <ion-row>
+            <ion-col>
+              <ion-card color="dark">
+                <ion-card-header>
+                  <ion-card-title>Ikan Hidup</ion-card-title>
+                </ion-card-header>
+                <ion-card-content>
+                  <h1 id="inline"><strong>182</strong></h1> Ekor
+                </ion-card-content>
+              </ion-card>
+            </ion-col>
+            <ion-col>
+              <ion-card color="dark">
+                <ion-card-header>
+                  <ion-card-title>Ikan Mati</ion-card-title>
+                </ion-card-header>
+                <ion-card-content>
+                  <h1 id="inline"><strong>89</strong></h1> Ekor
+                </ion-card-content>
+              </ion-card>
+            </ion-col>
+          </ion-row>
+          <ion-row>
+            <ion-col>
+              <ion-card color="dark">
+                <ion-card-header>
+                  <ion-card-title>Panen 2022</ion-card-title>
+                </ion-card-header>
+                <ion-card-content>
+                  <h1 id="inline"><strong>109</strong></h1> Kg
+                </ion-card-content>
+              </ion-card>
+            </ion-col>
+            <ion-col>
+              <ion-card color="dark">
+                <ion-card-header>
+                  <ion-card-title>Total Pakan</ion-card-title>
+                </ion-card-header>
+                <ion-card-content>
+                  <h1 id="inline"><strong>223</strong></h1> Kg
+                </ion-card-content>
+              </ion-card>
+            </ion-col>
+          </ion-row>
+          <ion-row>
+            <ion-col class="ion-padding">
+              <h1>Total Berat Ikan</h1>
+            </ion-col>
+          </ion-row>
+          <ion-row>
+            <ion-col>
+              <swiper :slides-per-view="2" :loop="true">
+                <swiper-slide>
+                  <ion-card color="dark">
+                    <ion-card-header>
+                      <ion-img src="img/lele.png"></ion-img>
+                    </ion-card-header>
+                    <ion-card-content>
+                      <h1><strong>Lele</strong></h1>
+                      <ion-text color="primary">
+                        <h1>200 Kg</h1>
+                      </ion-text>
+                    </ion-card-content>
+                  </ion-card>
+                </swiper-slide>
+                <swiper-slide>
+                  <ion-card color="dark">
+                    <ion-card-header>
+                      <ion-img src="img/nila.png"></ion-img>
+                    </ion-card-header>
+                    <ion-card-content>
+                      <h1><strong>Nila Merah</strong></h1>
+                      <ion-text color="primary">
+                        <h1>200 Kg</h1>
+                      </ion-text>
+                    </ion-card-content>
+                  </ion-card>
+                </swiper-slide>
+              </swiper>
+            </ion-col>
+          </ion-row>
+        </ion-grid>
+
+      </div>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script lang="ts">
+import { IonContent, IonPage, IonGrid, IonCol, IonRow, IonImg } from '@ionic/vue';
+import { defineComponent } from 'vue';
+import 'swiper/css';
+import '@ionic/vue/css/ionic-swiper.css';
+import { Swiper, SwiperSlide } from 'swiper/vue';
+
+export default defineComponent({
+  name: 'HomePage',
+  components: {
+    IonContent, IonPage, IonGrid, IonCol, IonRow, IonImg, Swiper, SwiperSlide
+  },
+  setup() {
+    // Optional parameters to pass to the swiper instance. See https://swiperjs.com/swiper-api for valid options.
+    return { }
+  }
+});
+</script>
+
+<style scoped>
+#inline {
+  display: inline;
+}
+
+</style>
---
+++ src/views/Tab2.vue
@@ -0,0 +1,245 @@
+<template>
+  <ion-page>
+    <ion-content :fullscreen="true">
+      <ion-item>
+        <ion-label>
+          <h1><strong>Kolam</strong></h1>
+        </ion-label>
+        <ion-icon src="img/swap.svg" slot="end" color="dark"></ion-icon>
+        <ion-icon src="img/sort.svg" slot="end" color="dark"></ion-icon>
+      </ion-item>
+
+      <ion-fab vertical="bottom" horizontal="end" slot="fixed">
+        <ion-fab-button>
+          <ion-icon src="img/add.svg" size="large"></ion-icon>
+        </ion-fab-button>
+      </ion-fab>
+
+      <ion-infinite-scroll>
+        <ion-infinite-scroll-content>
+          <ion-card color="dark">
+            <ion-item id="line" lines="inset" color="dark" class="ion-margin-horizontal">
+              <ion-label class="ion-padding-vertical"><strong>Alpha</strong></ion-label>
+              <ion-button slot="end" size="small" color="success">
+                <ion-text color="dark">
+                  Aktif
+                </ion-text>
+              </ion-button>
+            </ion-item>
+            <ion-card-content>
+              <ion-grid>
+                <ion-row class="ion-justify-content-start">
+                  <ion-col size="1">
+                    <ion-icon src="img/calendar.svg" id="inline"></ion-icon>
+                  </ion-col>
+                  <ion-col size="auto">
+                    <p>01-09-2022</p>
+                  </ion-col>
+                </ion-row>
+                <ion-row class="ion-justify-content-start">
+                  <ion-col size="1">
+                    <ion-icon src="img/alarm.svg" id="inline"></ion-icon>
+                  </ion-col>
+                  <ion-col size="auto">
+                    <p>90 hari</p>
+                  </ion-col>
+                </ion-row>
+                <ion-row class="ion-justify-content-start">
+                  <ion-col size="1">
+                    <ion-icon src="img/fish.svg" id="inline"></ion-icon>
+                  </ion-col>
+                  <ion-col size="auto">
+                    <p>209 ekor</p>
+                  </ion-col>
+                </ion-row>
+              </ion-grid>
+            </ion-card-content>
+          </ion-card>
+
+          <ion-card color="dark">
+            <ion-item id="line" lines="inset" color="dark" class="ion-margin-horizontal">
+              <ion-label class="ion-padding-vertical"><strong>Beta</strong></ion-label>
+              <ion-button slot="end" size="small" color="danger">
+                Tidak Aktif
+              </ion-button>
+            </ion-item>
+            <ion-card-content>
+              <ion-grid>
+                <ion-row class="ion-justify-content-start">
+                  <ion-col size="1">
+                    <ion-icon src="img/calendar.svg" id="inline"></ion-icon>
+                  </ion-col>
+                  <ion-col size="auto">
+                    <p>01-09-2022</p>
+                  </ion-col>
+                </ion-row>
+                <ion-row class="ion-justify-content-start">
+                  <ion-col size="1">
+                    <ion-icon src="img/alarm.svg" id="inline"></ion-icon>
+                  </ion-col>
+                  <ion-col size="auto">
+                    <p>90 hari</p>
+                  </ion-col>
+                </ion-row>
+                <ion-row class="ion-justify-content-start">
+                  <ion-col size="1">
+                    <ion-icon src="img/fish.svg" id="inline"></ion-icon>
+                  </ion-col>
+                  <ion-col size="auto">
+                    <p>209 ekor</p>
+                  </ion-col>
+                </ion-row>
+              </ion-grid>
+            </ion-card-content>
+          </ion-card>
+
+          <ion-card color="dark">
+            <ion-item id="line" lines="inset" color="dark" class="ion-margin-horizontal">
+              <ion-label class="ion-padding-vertical"><strong>Charlie</strong></ion-label>
+              <ion-button slot="end" size="small" color="success">
+                <ion-text color="dark">
+                  Aktif
+                </ion-text>
+              </ion-button>
+            </ion-item>
+            <ion-card-content>
+              <ion-grid>
+                <ion-row class="ion-justify-content-start">
+                  <ion-col size="1">
+                    <ion-icon src="img/calendar.svg" id="inline"></ion-icon>
+                  </ion-col>
+                  <ion-col size="auto">
+                    <p>01-09-2022</p>
+                  </ion-col>
+                </ion-row>
+                <ion-row class="ion-justify-content-start">
+                  <ion-col size="1">
+                    <ion-icon src="img/alarm.svg" id="inline"></ion-icon>
+                  </ion-col>
+                  <ion-col size="auto">
+                    <p>90 hari</p>
+                  </ion-col>
+                </ion-row>
+                <ion-row class="ion-justify-content-start">
+                  <ion-col size="1">
+                    <ion-icon src="img/fish.svg" id="inline"></ion-icon>
+                  </ion-col>
+                  <ion-col size="auto">
+                    <p>209 ekor</p>
+                  </ion-col>
+                </ion-row>
+              </ion-grid>
+            </ion-card-content>
+          </ion-card>
+
+          <ion-card color="dark">
+            <ion-item id="line" lines="inset" color="dark" class="ion-margin-horizontal">
+              <ion-label class="ion-padding-vertical"><strong>Delta</strong></ion-label>
+              <ion-button slot="end" size="small" color="success">
+                <ion-text color="dark">
+                  Aktif
+                </ion-text>
+              </ion-button>
+            </ion-item>
+            <ion-card-content>
+              <ion-grid>
+                <ion-row class="ion-justify-content-start">
+                  <ion-col size="1">
+                    <ion-icon src="img/calendar.svg" id="inline"></ion-icon>
+                  </ion-col>
+                  <ion-col size="auto">
+                    <p>01-09-2022</p>
+                  </ion-col>
+                </ion-row>
+                <ion-row class="ion-justify-content-start">
+                  <ion-col size="1">
+                    <ion-icon src="img/alarm.svg" id="inline"></ion-icon>
+                  </ion-col>
+                  <ion-col size="auto">
+                    <p>90 hari</p>
+                  </ion-col>
+                </ion-row>
+                <ion-row class="ion-justify-content-start">
+                  <ion-col size="1">
+                    <ion-icon src="img/fish.svg" id="inline"></ion-icon>
+                  </ion-col>
+                  <ion-col size="auto">
+                    <p>209 ekor</p>
+                  </ion-col>
+                </ion-row>
+              </ion-grid>
+            </ion-card-content>
+          </ion-card>
+
+          <ion-card color="dark">
+            <ion-item id="line" lines="inset" color="dark" class="ion-margin-horizontal">
+              <ion-label class="ion-padding-vertical"><strong>Epsilon</strong></ion-label>
+              <ion-button slot="end" size="small" color="success">
+                <ion-text color="dark">
+                  Aktif
+                </ion-text>
+              </ion-button>
+            </ion-item>
+            <ion-card-content>
+              <ion-grid>
+                <ion-row class="ion-justify-content-start">
+                  <ion-col size="1">
+                    <ion-icon src="img/calendar.svg" id="inline"></ion-icon>
+                  </ion-col>
+                  <ion-col size="auto">
+                    <p>01-09-2022</p>
+                  </ion-col>
+                </ion-row>
+                <ion-row class="ion-justify-content-start">
+                  <ion-col size="1">
+                    <ion-icon src="img/alarm.svg" id="inline"></ion-icon>
+                  </ion-col>
+                  <ion-col size="auto">
+                    <p>90 hari</p>
+                  </ion-col>
+                </ion-row>
+                <ion-row class="ion-justify-content-start">
+                  <ion-col size="1">
+                    <ion-icon src="img/fish.svg" id="inline"></ion-icon>
+                  </ion-col>
+                  <ion-col size="auto">
+                    <p>209 ekor</p>
+                  </ion-col>
+                </ion-row>
+              </ion-grid>
+            </ion-card-content>
+          </ion-card>
+        </ion-infinite-scroll-content>
+      </ion-infinite-scroll>
+      <div>
+      </div>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script lang="ts">
+import { IonContent, IonPage, IonInfiniteScroll, IonInfiniteScrollContent, IonCol, IonRow, IonFab, IonFabButton, IonText } from '@ionic/vue';
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'HomePage',
+  components: {
+    IonContent, IonPage, IonInfiniteScroll, IonInfiniteScrollContent, IonCol, IonRow, IonFab, IonFabButton, IonText
+  }
+});
+</script>
+
+<style scoped>
+#inline {
+  display: inline;
+}
+
+ion-card{
+  border-radius: 15px;
+}
+
+#line {
+  border-bottom: 3px solid black;
+}
+
+</style>
